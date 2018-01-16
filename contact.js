5ca7bea59cb0c994c05c13bb998f1c18fcca8529
smcshane <32510417+smcshane@users.noreply.github.com>
2018-01-16 15:48:53 +0000

    Business info

--- contact.js
+++ contact.js
@@ -1,9 +1,4 @@
 $(function () {
-
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
     $('#contact-form').validator();
 
 
@@ -21,7 +16,6 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    // data = JSON object that contact.php returns
 
                     // we recieve the type of the message: success x danger and apply it to the 
                     var messageAlert = 'alert-' + data.type;
@@ -32,9 +26,7 @@ $(function () {
                     
                     // If we have messageAlert and messageText
                     if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
                         $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
                         $('#contact-form')[0].reset();
                     }
                 }
